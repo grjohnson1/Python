35cb962d8073ad414923a53e2424b5fed4e7c222
grjohnson1 <grjohnson@gmail.com>
2025-06-20 20:20:31 -0600

    Python Playground (second edition) projects

---
+++ Python_Playground/README.md
@@ -0,0 +1,15 @@
+# Python_Playground
+
+## Description
+This covers projects from the ***Python Playground (Second Edition)*** by Mahesh Venkitachalam.
+
+# Preparing
+Go to main Python_Arena ReadMe file for steps on how to instill the information required to run the projects.
+
+# Python Scripts
+
+## drawTriangle.py
+Simple drawing of a Triangle where I personally added a color for each line.
+
+## koch.py
+This is the Koch Snowflake being drawn.
---
+++ Python_Playground/drawTriangle.py
@@ -0,0 +1,27 @@
+import turtle
+
+def draw_triangle(x1,y1, x2,y2, x3,y3, t):
+    # go to start of triangle
+    t.up()
+    t.setpos(x1,y1)
+    t.down()
+    t.color('green')
+    t.setpos(x2,y2)
+    t.color('red')
+    t.setpos(x3,y3)
+    t.color('blue')
+    t.setpos(x1,y1)
+    t.up()
+
+def main():
+    print('testing turtle graphics...')
+
+    t = turtle.Turtle()
+    t.hideturtle()
+
+    draw_triangle(-100,0, 0,-173.2, 100,0, t)
+    turtle.mainloop()
+
+# call main
+if __name__ == '__main__':
+    main()
